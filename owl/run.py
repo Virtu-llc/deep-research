eb01b30e1e970e70bf2c1d94dec1e8362121d936
aaron-zhlg <zhangligao9509@gmail.com>
2025-03-08 22:32:30 -0500

    run on headless mode

--- owl/run.py
+++ owl/run.py
@@ -45,7 +45,7 @@ def construct_society(question: str) -> OwlRolePlaying:
 
     tools_list = [
         *WebToolkit(
-            headless=False, # Set to True if you want to run in headless mode (e.g. on a remote server)
+            headless=True, # Set to True if you want to run in headless mode (e.g. on a remote server)
             web_agent_model=assistant_model, 
             planning_agent_model=assistant_model
         ).get_tools(),
